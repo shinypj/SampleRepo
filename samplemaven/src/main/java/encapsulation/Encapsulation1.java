e4ae15228f332507a5037feddfb640a0805dec27
shiny <shiny@ShinyPeterJohn>
2025-07-07 17:14:38 +1200

    Encapsulation commit

---
+++ samplemaven/src/main/java/encapsulation/Encapsulation1.java
@@ -0,0 +1,21 @@
+package encapsulation;
+
+public class Encapsulation1 {
+	
+	private String name;
+	private int age;
+	
+	public void setter(String name, int age)
+	{
+		this.name=name;
+		this.age=age;		
+	}
+	
+	public void getter()
+	{
+		System.out.println("Name:" + name);
+		System.out.println("Age:" + age);
+		
+	}
+
+}
---
+++ samplemaven/src/main/java/encapsulation/Encapsulation2.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Encapsulation2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Encapsulation1 obj = new Encapsulation1();
+		obj.setter("Serah", 2);
+		obj.getter();
+
+	}
+
+}
